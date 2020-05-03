68eb89027e99a38ab92ab272ce1714460fd1b295
scott mikul <scott.p.mikul@gmail.com>
2020-05-03 08:48:17 -0400

    Updates

---
+++ precache-manifest.8cf05efcc1c9893b50607bf80282c318.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d92afa48e201042a2bb5e6a209053edd",
+    "url": "/react-pages-test/index.html"
+  },
+  {
+    "revision": "f1586832ed1f0ee17aa8",
+    "url": "/react-pages-test/static/css/main.ac5d1542.chunk.css"
+  },
+  {
+    "revision": "652ae790720b6d1cbf34",
+    "url": "/react-pages-test/static/js/2.e31af3b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pages-test/static/js/2.e31af3b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1586832ed1f0ee17aa8",
+    "url": "/react-pages-test/static/js/main.86242b89.chunk.js"
+  },
+  {
+    "revision": "37402930c1fe5f98f7da",
+    "url": "/react-pages-test/static/js/runtime-main.d5bb117c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-pages-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ScottMikul/react-pages-test/precache-manifest.cf98cf5da7624a5f4a27ee9c2a45d7ac.js"
+  "/react-pages-test/precache-manifest.8cf05efcc1c9893b50607bf80282c318.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ScottMikul/react-pages-test/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-pages-test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
